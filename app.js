12e9bcdc9b200c6f85e38a7c5a28dd3cc6633287
Marlie Martinez <marlieahue@gmail.com>
2024-06-12 16:11:22 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// assemble this information using a single function that takes an id parameter and returns a Promise that resolves to an object containing specific data
+
+import { central, db1, db2, db3, vault } from "./databases.js";
+
+function getUserData(id) {
+  const dbs = {
+    db1: db1,
+    db2: db2,
+    db3: db3
+  };
+  return new Promise( resolve =>{})
+}
+
