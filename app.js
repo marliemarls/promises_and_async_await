5be6e4f379c0c52f5b43df5696bc3f7ba4044a3f
Marlie Martinez <marlieahue@gmail.com>
2024-06-14 12:25:26 -0400

    changed html file

--- app.js
+++ app.js
@@ -2,14 +2,21 @@
 
 import { central, db1, db2, db3, vault } from "./databases.js";
 
-const returnedValue = await Promise.resolve(vault(1));
-console.log(returnedValue)
+// const returnedValue = await Promise.resolve(vault(1));
+// console.log(returnedValue)
 
-function getUserData(id) {
+async function getUserData(id) {
   const dbs = {
     db1: db1,
     db2: db2,
-    db3: db3
+    db3: db3,
   };
-
+  const returnedValue = await Promise.resolve(vault(1));
+  console.log(returnedValue);
+  //added if statements that will immediately return when the id is not a number and when the id is outside of the range currently available
+  if (id < 1 && id > 10) return "Invalid input";
+  if (typeof id !== Number) return "Invalid input";
+  
 }
+
+console.log(await getUserData(true));
