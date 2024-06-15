a685ec58c9db98e9a30ab44effb219e0b145b7ae
Marlie Martinez <marlieahue@gmail.com>
2024-06-15 15:42:30 -0400

    worked on the function.... kind of getting somewhere

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ import { central, db1, db2, db3, vault } from "./databases.js";
 async function getUserData(id) {
   if (id < 1 && id > 10) return "Invalid input";
   if (typeof id !== "number") return "Invalid input";
-
+  const newObj = {};
   const dbs = {
     db1: db1,
     db2: db2,
     db3: db3,
   };
-  
 
+  const vaultValue = await Promise.resolve(vault(id));
+
+  const returnedValue = await Promise.resolve(central(id))
+  .then((database)=> dbs[database](id))
+  .then((companyInfo)=> companyInfo)
+
+  console.log(returnedValue)
+
+  return vaultValue
+  
 
 }
 
-console.log(getUserData(1));
+console.log(await getUserData(1));
